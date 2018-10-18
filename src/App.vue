3b4b9b75fe51eb8d62edcbdcc2e855e43d5351e5
Madyan Eka <madyan.eka95@gmail.com>
2018-10-18 10:07:24 +0700

    posting data to a server (post request)

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,12 @@
 
         methods: {
             submit() {
-                console.log(this.user)
+                this.$http.post('https://vuejs-http-f15ce.firebaseio.com/data.json', this.user)
+                    .then((response) => {
+                        console.log(response)
+                    }, (error) => {
+                        console.log(error)
+                    })
             }
         }
     }
